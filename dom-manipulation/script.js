8e12e6e41aedfb999cb66caae2f86e44677c5884
adepegba1 <adepegbadavid@rocketmail.com>
2025-10-17 10:17:22 +0100

    0. Building a Dynamic Content Generator with Advanced DOM Manipulation

---
+++ dom-manipulation/script.js
@@ -0,0 +1,60 @@
+const submitBtn = document.getElementById("newQuote");
+const quoteDisplay = document.getElementById("quoteDisplay");
+
+let quotes = [
+  {
+    category: "Motivation",
+    quote: "Believe you can and you're halfway there.",
+  },
+  {
+    category: "Success",
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+  },
+  {
+    category: "Happiness",
+    quote:
+      "Happiness is not something ready made. It comes from your own actions.",
+  },
+  {
+    category: "Inspiration",
+    quote:
+      "You don't have to be great to start, but you have to start to be great.",
+  },
+  {
+    category: "Resilience",
+    quote: "Life is 10% what happens to you and 90% how you react to it.",
+  },
+];
+
+function createAddQuoteForm() {
+  const newQuoteText = document.getElementById("newQuoteText");
+  const newQuoteCategory = document.getElementById("newQuoteCategory");
+  if (newQuoteCategory.value !== "" && newQuoteText.value !== "") {
+    const newQuote = {
+      category: newQuoteCategory.value.trim(),
+      quote: newQuoteText.value.trim(),
+    };
+    quotes.push(newQuote);
+    newQuoteCategory.value = "";
+    newQuoteText.value = "";
+    newQuoteText.focus();
+    quoteDisplay.textContent = showRandomQuote();
+  }
+}
+
+function showRandomQuote() {
+  if (quotes.length > 0) {
+    const rand = Math.floor(Math.random() * quotes.length);
+    return `${quotes[rand].category}: "${quotes[rand].quote}"`;
+  } else {
+    return "No quotes available";
+  }
+}
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  quoteDisplay.textContent = showRandomQuote();
+});
+
+function addQuote() {
+  createAddQuoteForm();
+}
