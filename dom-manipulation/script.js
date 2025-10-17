591923d2c423ee37ca37cb921b91fb2f6d76fca2
adepegba1 <adepegbadavid@rocketmail.com>
2025-10-17 10:20:50 +0100

    0. Building a Dynamic Content Generator with Advanced DOM Manipulation

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,25 +4,23 @@ const quoteDisplay = document.getElementById("quoteDisplay");
 let quotes = [
   {
     category: "Motivation",
-    quote: "Believe you can and you're halfway there.",
+    text: "Believe you can and you're halfway there.",
   },
   {
     category: "Success",
-    quote: "It does not matter how slowly you go as long as you do not stop.",
+    text: "It does not matter how slowly you go as long as you do not stop.",
   },
   {
     category: "Happiness",
-    quote:
-      "Happiness is not something ready made. It comes from your own actions.",
+    text: "Happiness is not something ready made. It comes from your own actions.",
   },
   {
     category: "Inspiration",
-    quote:
-      "You don't have to be great to start, but you have to start to be great.",
+    text: "You don't have to be great to start, but you have to start to be great.",
   },
   {
     category: "Resilience",
-    quote: "Life is 10% what happens to you and 90% how you react to it.",
+    text: "Life is 10% what happens to you and 90% how you react to it.",
   },
 ];
 
@@ -32,7 +30,7 @@ function createAddQuoteForm() {
   if (newQuoteCategory.value !== "" && newQuoteText.value !== "") {
     const newQuote = {
       category: newQuoteCategory.value.trim(),
-      quote: newQuoteText.value.trim(),
+      text: newQuoteText.value.trim(),
     };
     quotes.push(newQuote);
     newQuoteCategory.value = "";
